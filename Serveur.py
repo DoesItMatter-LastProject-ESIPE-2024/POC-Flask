b93f853ff7d13676c2570742260ca4ab2e6b67af
Ornlu14 <logan.beaury@free.fr>
2024-02-20 18:43:50 +0100

    update: swagger

--- Serveur.py
+++ Serveur.py
@@ -6,9 +6,15 @@ from flask import Flask #serveur web python
 from swagger_ui import api_doc
 
 import random
+import os
+
+spec_string = '{"openapi":"3.0.1","info":{"title":"python-swagger-ui test api","description":"python-swagger-ui test api","version":"1.0.0"},"servers":[{"url":"http://127.0.0.1:8989/api"}],"tags":[{"name":"default","description":"default tag"}],"paths":{"/hello/world":{"get":{"tags":["default"],"summary":"output hello world.","responses":{"200":{"description":"OK","content":{"application/text":{"schema":{"type":"object","example":"Hello World!!!"}}}}}}}},"components":{}}'
+
 
 app = Flask(__name__)
-api_doc(app, config_path='./swagger.yml', url_prefix='/api', title='API DIM')
+working_dir = os.path.dirname(os.path.abspath(__file__))
+conf_path = os.path.join(working_dir, 'config/swagger.yml')
+api_doc(app, config_path=conf_path, url_prefix='/api/doc', title='API DIM', )
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello_world():
@@ -18,10 +24,10 @@ def hello_world():
 def display_random_list():
     return random_list()
 
-def random_list():
+def random_list():    
     list = []
     for i in range(0,5):
         list.append(random.randint(1,10))
     return list
 
-serve(app, host='0.0.0.0', port=30080)
+serve(app, host='0.0.0.0', port=8080)
