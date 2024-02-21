8820c619628bab28a5aeceba45f5fe94a748b53a
Ornlu14 <logan.beaury@free.fr>
2024-02-21 09:52:48 +0100

    update: swagger

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import Flask #serveur web python
 from swagger_ui import api_doc
 
 import random
+import socket
 import os
 
 spec_string = '{"openapi":"3.0.1","info":{"title":"python-swagger-ui test api","description":"python-swagger-ui test api","version":"1.0.0"},"servers":[{"url":"http://127.0.0.1:8989/api"}],"tags":[{"name":"default","description":"default tag"}],"paths":{"/hello/world":{"get":{"tags":["default"],"summary":"output hello world.","responses":{"200":{"description":"OK","content":{"application/text":{"schema":{"type":"object","example":"Hello World!!!"}}}}}}}},"components":{}}'
@@ -14,9 +15,10 @@ spec_string = '{"openapi":"3.0.1","info":{"title":"python-swagger-ui test api","
 app = Flask(__name__)
 working_dir = os.path.dirname(os.path.abspath(__file__))
 conf_path = os.path.join(working_dir, 'config/swagger.yml')
-api_doc(app, config_path=conf_path, url_prefix='/api/doc', title='API DIM', )
 
-@app.route('/', methods = ['GET', 'POST'])
+api_doc(app, config_path=conf_path, url_prefix='/api/doc', title='API DIM')
+
+@app.route('/')
 def hello_world():
     return "<h1> Hello World! </h1>"
 
@@ -24,7 +26,8 @@ def hello_world():
 def display_random_list():
     return random_list()
 
-def random_list():    
+def random_list():
+    print(f"{socket.getaddrinfo}")    
     list = []
     for i in range(0,5):
         list.append(random.randint(1,10))
