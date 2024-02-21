4d2e43470250e70696228c0437a65b7fbc1d1af0
Ornlu14 <logan.beaury@free.fr>
2024-02-21 10:41:24 +0100

    update: refacto

--- server.py
+++ server.py
@@ -5,14 +5,14 @@ from waitress import serve #serveur de production
 from flask import Flask #serveur web python
 from swagger_ui import api_doc
 
+import json
 import random
-import socket
 import os
 
 spec_string = '{"openapi":"3.0.1","info":{"title":"python-swagger-ui test api","description":"python-swagger-ui test api","version":"1.0.0"},"servers":[{"url":"http://127.0.0.1:8989/api"}],"tags":[{"name":"default","description":"default tag"}],"paths":{"/hello/world":{"get":{"tags":["default"],"summary":"output hello world.","responses":{"200":{"description":"OK","content":{"application/text":{"schema":{"type":"object","example":"Hello World!!!"}}}}}}}},"components":{}}'
 
-
 app = Flask(__name__)
+
 working_dir = os.path.dirname(os.path.abspath(__file__))
 conf_path = os.path.join(working_dir, 'config/swagger.yml')
 
@@ -24,13 +24,9 @@ def hello_world():
 
 @app.route('/test')
 def display_random_list():
-    return random_list()
-
-def random_list():
-    print(f"{socket.getaddrinfo}")    
     list = []
     for i in range(0,5):
         list.append(random.randint(1,10))
-    return list
+    return json.dumps(list)     
 
 serve(app, host='0.0.0.0', port=8080)
